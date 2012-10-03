b1a6af8900a9cacbeb45f8f475ab4496cef9fd76
Ilya Yanok <ilya.yanok@cogentembedded.com>
2012-10-03 19:41:25 +0400

    musb-new: omap_musb: add interface type field
    Add interface_type field to omap_musb board data.
    
    Signed-off-by: Ilya Yanok <yanok@cogentembedded.com>

--- arch/arm/include/asm/omap_musb.h
+++ arch/arm/include/asm/omap_musb.h
@@ -21,8 +21,11 @@ extern struct musb_platform_ops musb_dsps_ops;
 extern const struct musb_platform_ops am35x_ops;
 
 struct omap_musb_board_data {
+	u8 interface_type;
 	void (*set_phy_power)(u8 on);
 	void (*clear_irq)(void);
 	void (*reset)(void);
 };
+
+enum musb_interface    {MUSB_INTERFACE_ULPI, MUSB_INTERFACE_UTMI};
 #endif /* __ASM_ARM_OMAP_MUSB_H */
